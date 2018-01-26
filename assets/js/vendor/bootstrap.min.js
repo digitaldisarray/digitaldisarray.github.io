645be3d06e8d92fadf34437e685e21422ade2d03
digitaldisarray <electrichexagon8@gmail.com>
2018-01-25 17:47:53 -0800

    Added website.

---
+++ assets/js/vendor/bootstrap.min.js
@@ -0,0 +1,130 @@
+<!DOCTYPE html PUBLIC
+    "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>bplaced - Webspace &amp; Webhosting // 2GB Freehost :: The place for your webspace</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="Content-Language" content="de" />
+        <meta http-equiv="Content-Style-Type" content="text/css" />
+        <meta name="Robots" content="INDEX, FOLLOW" />
+        <meta name="Copyright" content="miro" />
+        <meta http-equiv="cache-control" content="max-age=0" />
+        <meta http-equiv="cache-control" content="no-cache" />
+        <meta http-equiv="expires" content="0" />
+        <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+        <meta http-equiv="pragma" content="no-cache" />
+        <link rel="shortcut icon" href="//www.bplaced.net/favicon.ico" />
+        <link rel="stylesheet" href="//www.bplaced.net/epage.css" type="text/css" />
+    </head>
+    <body>
+        <div id="head">
+            <div id="message" class="bside_top">
+                <div class="bside" style="width: 700px;">
+                    Diese Seite ist leider nicht mehr verfügbar oder noch nicht aktiv.<span>Sorry, this page is not available any more, or pending.</span> 
+                </div>
+                <div class="bside" style="width: 100px;">
+                    Fehler
+                    <span>Error</span>
+                </div>
+                <div class="bside" id="errno">
+                    404
+                </div>
+            </div>
+        </div>
+
+        <div id="content" class="bside_top">
+            <div id="title">
+                <div class="bside" style="width: 250px;">
+                    <img alt="" src="//www.bplaced.net/gfx/bpt.gif" />
+                </div>
+                <div class="bside" style="width: 690px;">
+                    <h1>Willkommen bei bplaced.net - Webspace &amp; Webhosting</h1>
+                </div>
+            </div>
+        </div>
+
+        <div id="center" class="bside_top">
+            <div class="bside separator">
+                <a target="_parent" href="http://www.bplaced.net" class="square">
+                    <br />
+                    <h3>freestyle</h3>
+                    <h2>1GB</h2>
+                    8 FTP-Konten <br />
+                    8 MySQL-Datenbanken <br />
+                    8 PostgreSQL-Datenbanken <br />
+                    <h3>kostenlos</h3>
+                    <span>free</span>
+                </a>
+                <h2>Über bplaced</h2>
+                <br />
+                <div style="font: 14px/1.4em Verdana, sans-serif;">
+                    bplaced ist einer der führenden Hosting-Provider
+                    im Bereich Freehosting in <br /> Europa seit 2007. <br />
+                    <br />
+                    Mit einer Fülle an Funktionen stehen Einsteigern
+                    unsere Angebote <span style="color: #0a5fb2">freestyle</span>
+                    und <span style="color: #0a5fb2">freestyle plus</span> selbstverständlich
+                    <b>werbefrei</b> zur Verfügung. <br />
+                    <br />
+                    Für fortgeschrittene Nutzer bieten wir <br /> mit unserer Erweiterung
+                    <span style="color: #159de7">bplaced pro</span> eine Lösung für anspruchsvolle
+                    Projekte in einem der modernsten Netzwerke Europas!
+                </div>
+            </div>
+            <div class="bside separator">
+                <a target="_parent" href="http://www.bplaced.net" class="square">
+                    <br />
+                    <h3>freestyle plus</h3>
+                    <h2>2GB</h2>
+                    2 FTP-Konten <br />
+                    2 MySQL-Datenbanken <br />
+                    2 PostgreSQL-Datenbanken <br />
+                    <h3>kostenlos</h3>
+                    <span>free</span>
+                </a>
+                <h2>Hilfe und Support</h2>
+                <br />
+                <div style="font: 14px/1.4em Verdana, sans-serif;">
+                    Auf unserem <a target="_parent" href="http://eass.bplaced.net/">Hilfsportal EASS</a> findest Du viele
+                    Artikel rund ums Thema Webmastering.
+                    <br />
+                    <ul type="square" style="padding-left: 14px;">
+                        <li>Domains<div>Erklärungen und Anleitungen</div></li>
+                        <li>FTP<div>Dateien und Ordner hochladen</div></li>
+                        <li>Datenbanken<div>PostgreSQL und MySQL</div></li>
+                        <li>Auszeichnungs- &amp; Programmiersprachen<div>Hilfe zu HTML, PHP &amp; Co</div></li>
+                        <li>Installationsanleitungen<div>Wordpress, Joomla, etc. auf bplaced installieren</div></li>
+                    </ul> 
+                </div>
+            </div>
+            <div class="bside separator" style="width: 230px; padding-right: 2px;">
+                <img style="position: absolute; margin:  68px 0 0 196px;" alt="" src="//www.bplaced.net/gfx/efeat.png" />
+                <a target="_parent" href="http://www.bplaced.net" class="square squarepro">
+                    <br />
+                    <h3>pro</h3>
+                    <h2>4GB</h2>
+                    16 FTP-Konten <br />
+                    16 MySQL-Datenbanken <br />
+                    16 PostgreSQL-Datenbanken <br />
+                    <img alt="" src="//www.bplaced.net/gfx/epr.png" />
+                </a>
+                <h2>Promotions</h2>
+                <div id="promotions">
+                     <script data-cfasync="false" type="text/javascript" src="http://www.tradeadexchange.com/a/display.php?r=421222"></script> 
+                     <script data-cfasync="false" type="text/javascript" src="http://www.tradeadexchange.com/a/display.php?r=422682"></script> 
+                </div>
+            </div>
+        </div>
+
+        <div id="footer">
+            <div id="grad"><img alt="" src="//www.bplaced.net/gfx/grad.png" /></div>
+            <div class="bside_top">
+                <div class="bside" style="width: 70%; height: 20px;"><b><span style="color: #0b5fb4;">b</span>placed</b>&trade; Webhosting 2007 - 2015 | 2GB kostenloser Webspace | Business Hosting</div>
+                <div class="bside" style="width: 30%; text-align: right;"><a target="_parent" href="http://www.bplaced.net/?location=contact">Contact Service Provider</a></div>
+            </div>
+        </div>
+        <br />
+        &nbsp;
+    </body>
+</html>
